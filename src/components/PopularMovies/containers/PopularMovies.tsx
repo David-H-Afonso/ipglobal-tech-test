ceb9299414606b76ee88580c8695978ec17397c9
David Hormiga Afonso <davidhormigafonso@gmail.com>
2024-10-05 20:11:46 +0200

    feat: show user rating on popular movies

--- src/components/PopularMovies/containers/PopularMovies.tsx
+++ src/components/PopularMovies/containers/PopularMovies.tsx
@@ -4,8 +4,10 @@ import PopularMoviesComponent from '../components/PopularMoviesComponent'
 const PopularMovies = () => {
 	const movies = useAppSelector((state) => state.popularMovies.value)
 	const genres = useAppSelector((state) => state.genres.value)
+	// To check if a movie has been rated and show rating
+	const ratedMovies = useAppSelector((state) => state.ratedMovies.value)
 
-	return <PopularMoviesComponent movies={movies} genres={genres} />
+	return <PopularMoviesComponent movies={movies} genres={genres} ratedMovies={ratedMovies} />
 }
 
 export default PopularMovies
